b6e8085ce53e7db2341fa5727942df4aed98faa4
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-08 18:07:21 +0200

    configure custom cell

--- fashion-app/ViewController.swift
+++ fashion-app/ViewController.swift
@@ -7,7 +7,38 @@
 
 import UIKit
 
+struct  OnboardingItem {
+    let title : String
+    let detail : String
+    let image : UIImage?
+}
+
+
+
+
+
 class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    
+    //MARK: - properties
+    private let items: [OnboardingItem] = [
+        .init(title: "Diana Vreeland",
+              detail: "Fashion is part of the daily air and it changes all the time, with all the events. You can even see the approaching of a revolution in clothes. You can see and feel everything in clothes.",
+              image: UIImage(named: "imFashion1")),
+        .init(title: "Gianni Versace",
+              detail: "Don't be into trends. Don't make fashion own you, but you decide what you are, what you want to express by the way you dress and the way to live.",
+              image: UIImage(named: "imFashion2")),
+        .init(title: "Karl Lagerfeld",
+              detail: "One is never over-dressed or under-dressed with a Little Black Dress.",
+              image: UIImage(named: "imFashion3")),
+        .init(title: "Miuccia Prada",
+              detail: "What you wear is how you present yourself to the world, especially today, when human contacts are so quick. Fashion is instant language.",
+              image: UIImage(named: "imFashion4")),
+        .init(title: "Bette Midler",
+              detail: "I firmly believe that with the right footwear one can rule the world.",
+              image: UIImage(named: "imFashion5"))
+    ]
+    
+    
     //MARK: - IBOutlets ans IBAction
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var containerView: UIView!
@@ -34,13 +65,15 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     
     //MARK: - DataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .blue
-        return cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! FashionCustomCell
+        let item = items[indexPath.item]
+        
+        cell.configure(with: item)
+    return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -58,5 +91,10 @@ class FashionCustomCell : UICollectionViewCell {
     @IBOutlet weak var titleLable: UILabel!
     
     @IBOutlet weak var secondTitleLable: UILabel!
+    
+    func configure(with item: OnboardingItem){
+        titleLable.text = item.title
+        secondTitleLable.text = item.detail
+    }
 }
 
