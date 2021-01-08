d27e097bcf34cb8fdde7b1a315a8f2176ed828a8
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-08 16:47:16 +0200

    add extension

--- fashion-app/ViewController.swift
+++ fashion-app/ViewController.swift
@@ -7,9 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource{
-    
-    
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
     //MARK: - IBOutlets ans IBAction
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var containerView: UIView!
@@ -17,9 +15,6 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     @IBOutlet weak var collectionView: UICollectionView!
     @IBAction func nextButtonAction(_ sender: Any) {
     }
-    
-    
-    
     //MARK: - functions
     
     private func setupCollectionView(){
@@ -30,17 +25,30 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         collectionView.collectionViewLayout = layout
         collectionView.isPagingEnabled = true
     }
-    
-    
-    
-    
-    
     //MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCollectionView()
     }
 
-
+    
+    //MARK: - DataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
 }
 
