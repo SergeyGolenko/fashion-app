ecaaec3d4743de8cb0e3cc9932027968b6d20fad
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-08 13:16:56 +0200

    Initial Commit

---
+++ fashion-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fashion-app
+//
+//  Created by Сергей Голенко on 08.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
