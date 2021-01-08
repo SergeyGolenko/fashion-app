bbbe318a1887b93128b88d223d4507179e50fd65
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-08 16:25:00 +0200

    add setupCollectionView func

--- fashion-app/ViewController.swift
+++ fashion-app/ViewController.swift
@@ -7,8 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    
+    //MARK: - IBOutlets ans IBAction
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet var pageControl: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBAction func nextButtonAction(_ sender: Any) {
+    }
+    
+    
+    
+    //MARK: - functions
+    
+    private func setupCollectionView(){
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = layout
+        collectionView.isPagingEnabled = true
+    }
+    
+    
+    
+    
+    
+    //MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
